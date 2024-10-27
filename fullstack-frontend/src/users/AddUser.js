c5af0d46778737ede77e444e46f1873012c62f60
rm1710 <d290mishraritesh@gmail.com>
2024-10-27 21:29:07 +0530

    commit all

--- fullstack-frontend/src/users/AddUser.js
+++ fullstack-frontend/src/users/AddUser.js
@@ -21,8 +21,7 @@ export default function AddUser() {
     const onSubmit = async (e) => {
         e.preventDefault();
         await axios.post("http://localhost:8080/user", user);  // Use users
-        navigate("/");
-        
+        navigate("/");   
     };
 
     return (
